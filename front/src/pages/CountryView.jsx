9ff196b3bd2dacbcadeff3b3babdcfb9f4a1e91a
GonzaloAgu <gonzalo.ag88@gmail.com>
2024-10-24 16:14:40 -0300

    fix: every country load an argentina flag

--- front/src/pages/CountryView.jsx
+++ front/src/pages/CountryView.jsx
@@ -1,18 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState} from "react";
+import { useParams } from "react-router-dom";
 import Header from "../components/Header";
 import { Image } from "react-bootstrap";
 
 export default function CountryView() {
 
     const [country, setCountry] = useState(null)
+    const { countryCode } = useParams();
 
-    const API_COUNTRY_URL = import.meta.env.VITE_API_BASE_URL + ":" + import.meta.env.VITE_API_PORT + "/country/AR"
+    const API_COUNTRY_URL = import.meta.env.VITE_API_BASE_URL + ":" + import.meta.env.VITE_API_PORT + "/country/" + countryCode;
 
     useEffect(() => {
         fetch(API_COUNTRY_URL)
             .then(res => res.json())
             .then(data => {
                 setCountry(data)
+            })
+            .then(() => {
             })
             .catch(err => console.error(err))
     }, [])
