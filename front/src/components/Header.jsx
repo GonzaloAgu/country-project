38b5d178b01ee53a10b6d4f8477dcfce41e46ebd
GonzaloAgu <gonzalo.ag88@gmail.com>
2024-10-25 09:58:33 -0300

    visual improvements

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,11 +1,18 @@
 import React from "react"
+import { Navbar, Container } from "react-bootstrap"
 
-export default function Header({title}){
+export default function Header({ title }) {
     return (
-        <div className="mb-5 p-3 d-flex justify-content-center flex-column">
-            <h1>{title}</h1>
-            <h3 className="text-secondary">Click on any country to know more about it!</h3>
-        </div>
+        <>
+            <Navbar className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href="/" className="hoverable">Home</Navbar.Brand>
+                </Container>
+            </Navbar>
+            {title? <div className="mb-5 p-3 d-flex justify-content-center flex-column">
+                <h1>{title}</h1>
+            </div> : <div className="p-2"></div>}
+        </>
     )
 }
 
--- front/src/pages/CountryView.jsx
+++ front/src/pages/CountryView.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Header from "../components/Header";
-import { Image, Alert } from "react-bootstrap";
+import { Image, Alert, Spinner } from "react-bootstrap";
 import CountryList from "../components/CountryList";
 import PopulationChart from "../components/PopulationChart"
 
@@ -9,6 +9,7 @@ export default function CountryView() {
 
     const [country, setCountry] = useState(null)
     const { countryCode } = useParams();
+    const [loading, setLoading] = useState(true)
     const [error404, setError404] = useState(false)
 
     const API_COUNTRY_URL = import.meta.env.VITE_API_BASE_URL + ":" + import.meta.env.VITE_API_PORT + "/country/" + countryCode;
@@ -17,24 +18,33 @@ export default function CountryView() {
         fetch(API_COUNTRY_URL)
             .then(res => res.json())
             .then(data => {
-                if(data?.status === 404) {
+                if (data?.status === 404) {
                     setError404(true);
                     return;
                 }
+                setLoading(false);
                 setCountry(data)
             })
             .catch(err => setError404(true))
     }, [])
 
-    if(error404)
+    if (loading) {
+        return <>
+            <Header></Header>
+            <Spinner animation="grow" />
+        </>
+    }
+
+    if (error404)
         return (
             <Alert variant={"danger"}>The country with {countryCode} code couldn't be reached.</Alert>
-    )
+        )
 
 
     return country ? (
         <>
-            <div>
+            <Header></Header>
+            <div className="pb-3">
                 <Image src={country.flag} thumbnail width={200} className="mb-1"></Image>
                 <h1>
                     {country.commonName}
@@ -43,17 +53,21 @@ export default function CountryView() {
                     {country.officialName}
                 </h4>
             </div>
-            <div className="mt-4">
-                <h2>
-                    Bordering countries
-                </h2>
-                <CountryList countries={country.borders}></CountryList>
-            </div>
-            <div className="mt-4">
+            <div className="py-3 mt-4">
                 <h2>
                     Population over time
                 </h2>
                 <PopulationChart data={country.populationCounts}></PopulationChart>
+            </div>
+            <div className="py-3 mt-4">
+                <h2 className="mb-4">
+                    Bordering countries
+                </h2>
+                {country.borders.length === 0 ?
+                    <p>This country has no borders!</p> :
+
+                    <CountryList countries={country.borders}></CountryList>
+                }
             </div>
         </>
     ) : null;
